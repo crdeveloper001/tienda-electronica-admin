6953c40b83de5ca6811123834b2a5fa6ef2c92fa
crdeveloper001 <claudiogh33@gmail.com>
2023-05-13 00:34:01 -0600

    .

--- src/app/services/authorization.service.ts
+++ src/app/services/authorization.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IAuthorization } from '../interfaces/Authorization';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
 
-  constructor() { }
+  private readonly API_SERVER:string = "https://localhost:7149/api/Authorization";
+
+  constructor(private request:HttpClient) { }
+
+  PostAuthorization(credentials:IAuthorization):Observable<IAuthorization>{
+    return this.request.post<IAuthorization>(this.API_SERVER,credentials);
+  }
 }
--- src/app/services/user-accounts.service.ts
+++ src/app/services/user-accounts.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IAuthorization } from '../interfaces/Authorization';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserAccountsService {
 
-  constructor() { }
+  
 }
