93190edf666302f29a2aaa1de3e9e0d28e7c7bda
crdeveloper001 <claudiogh33@gmail.com>
2023-05-13 19:43:55 -0600

    service authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { LoginComponent } from './components/login/login.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { IAuthorization } from "src/app/interfaces/Authorization";
+import { AuthorizationService } from "src/app/services/authorization.service";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  credentials: IAuthorization;
 
-  
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private service: AuthorizationService,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      usernameInput: new FormControl("", Validators.required),
+      passwordInput: new FormControl("", Validators.required),
+    });
+    this.credentials = {password:'',username:''}
   }
 
+  async GenerateAuth() {
+   
+    this.credentials = {
+      username:this.loginForm.get('usernameInput')?.value,
+      password:this.loginForm.get('passwordInput')?.value
+    }
+    
+    await this.service.PostAuthorization(this.credentials).subscribe(
+      (result: any) => {
+        console.log(result);
+        
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.error.error.message);
+      }
+    );
+  }
+
+  ngOnInit(): void {}
 }
